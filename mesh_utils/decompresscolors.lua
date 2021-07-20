6b5abc0733bc7d3104facb5cb8449e2117c80a5e
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2021-07-20 17:36:33 -0500

    Fixed a bug and reformatted to better match luarocks

--- mesh_utils/decompresscolors.lua
+++ mesh_utils/decompresscolors.lua
@@ -1,22 +1,24 @@
 function decompresscolors(meshlist)
-  for meshindex = 1, #meshlist do
-    local mesh = meshlist[meshindex]
-    local colors = mesh.colors
-    mesh.colors = {}
-    for color, group in pairs(colors) do
-      for groupitemindex = 1,  #group do
-        local groupitem = group[groupitemindex]
-        if type(groupitem)=="table" then
-          for i = groupitem[1], groupitem[2] do
-            mesh.colors[i] = color
-          end
-        elseif type(groupitem)=="number" then
-          mesh.colors[groupitem] = color
-        end
+   for meshindex = 1, #meshlist do
+      local mesh = meshlist[meshindex]
+      if mesh.colors~=nil then
+         local colors = mesh.colors
+         mesh.colors = {}
+         for color, group in pairs(colors) do
+            for groupitemindex = 1,  #group do
+               local groupitem = group[groupitemindex]
+               if type(groupitem)=="table" then
+                  for i = groupitem[1], groupitem[2] do
+                     mesh.colors[i] = color
+                  end
+               elseif type(groupitem)=="number" then
+                  mesh.colors[groupitem] = color
+               end
+            end
+         end
+         meshlist[meshindex] = mesh
       end
-    end
-    meshlist[meshindex] = mesh
-  end
-  return meshlist
+   end
+   return meshlist
 end
 return decompresscolors
