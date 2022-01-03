1a98a46c1e98200963d54916b35b53afd8ffb353
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2022-01-03 12:34:25 -0600

    Merged decompresscolors.lua and decompressmeshes.lua

--- mesh_utils/decompressmeshes.lua
+++ mesh_utils/decompressmeshes.lua
@@ -6,6 +6,23 @@ function decompressmeshes(meshlist)
       end
     end
   end
+  for meshindex, mesh in ipairs(meshlist) do
+    if mesh.colors~=nil then
+      local colors = mesh.colors
+      mesh.colors = {}
+      for color, group in pairs(colors) do
+        for groupitemindex, groupitem in ipairs(group) do
+          if type(groupitem)=="table" then
+            for i = groupitem[1], groupitem[2] do
+              mesh.colors[i+1] = color
+            end
+          elseif type(groupitem)=="number" then
+            mesh.colors[groupitem+1] = color
+          end
+        end
+      end
+    end
+  end
   return meshlist
 end
 return decompressmeshes
